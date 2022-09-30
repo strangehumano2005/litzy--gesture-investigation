61ff85bf12c8a2f95ef79defe08658d295aac27d
strangehumano2005 <112585795+strangehumano2005@users.noreply.github.com>
2022-09-30 08:50:21 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showIcon(IconNames.House)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showIcon(IconNames.Angry)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showIcon(IconNames.EigthNote)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showIcon(IconNames.QuarterNote)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showIcon(IconNames.No)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showIcon(IconNames.Ghost)
+})
